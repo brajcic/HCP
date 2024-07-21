398e6294a81718d01bcfb1616fea97ff2fa4a242
Bojan <47784425+brajcic@users.noreply.github.com>
2024-07-21 09:53:05 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = ">= 1.0.0"
+}
+
+# Documentation: https://registry.terraform.io/providers/hashicorp/local/latest/docs/resources/file
+
+resource "local_file" "changeme_local_file_hello" {
+  content  = "Hello terraform local!"
+  filename = "${path.module}/changeme_local_file_hello_${terraform.workspace}.txt"
+}
